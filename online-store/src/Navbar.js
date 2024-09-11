f6408314af1e091ab5bbb78a2352e7d96acc8688
Anuj2106 <cseanuj9505@gmail.com>
2024-09-11 15:49:27 +0530

    hth

--- online-store/src/Navbar.js
+++ online-store/src/Navbar.js
@@ -7,23 +7,25 @@ function BasicExample() {
     return (
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+          {/* <img
+              src="/img/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            /> */}
+            Online-Store 
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="mx-auto">
+              <Nav.Link href="#home">Shop</Nav.Link>
+              <Nav.Link href="#link">Men</Nav.Link>
+              <Nav.Link href="#link">Women</Nav.Link>
+              <Nav.Link href="#link"> </Nav.Link>
+              <Nav.Link href="#link">Women</Nav.Link>
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
